5c24c9e97d9e0c15b4736f4d04e3e22a4d166362
eniompw <71254710+eniompw@users.noreply.github.com>
2025-11-10 12:08:00 +0000

    init

---
+++ search.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://zoopla-uk.p.rapidapi.com/properties/search-sale"
+querystring = {
+    "query": "South Kensington"
+}
+
+headers = {
+    "x-rapidapi-host": "zoopla-uk.p.rapidapi.com",
+    "x-rapidapi-key": ""  # Replace with your actual RapidAPI key
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+data = response.json()
+print(data['data']['listings']['regular'][0].keys())
+print(data['data']['listings']['regular'][0]['address'])
